450c4ae24413b3155301313b5c980dacd19cf1cb
sam <sam.charles.stanley@gmail.com>
2025-04-17 11:49:44 -0700

    added some starter code in the application wrapper

---
+++ src/wrapper/program_wrapper.cpp
@@ -0,0 +1,17 @@
+#include "./program_wrapper.hpp"
+
+// MARK: private methods
+
+program_wrapper::program_wrapper() {
+}
+
+// MARK: public methods
+
+program_wrapper& program_wrapper::get_instance(void) {
+    static program_wrapper instance;
+    return instance;
+}
+
+void program_wrapper::run_program(const int arg_count, const char* const* arg_vector) {
+    std::cout << "here" << std::endl;
+}
---
+++ src/wrapper/program_wrapper.hpp
@@ -0,0 +1,16 @@
+#ifndef __PROGRAM_WRAPPER_HPP__
+#define __PROGRAM_WRAPPER_HPP__
+
+#include <iostream>
+
+class program_wrapper {
+private:
+    program_wrapper();
+public:
+    program_wrapper(const program_wrapper&) = delete;
+    program_wrapper& operator=(const program_wrapper&) = delete;
+    static program_wrapper& get_instance(void);
+    void run_program(const int arg_count, const char* const* arg_vector);
+};
+
+#endif
