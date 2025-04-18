2c40d010a29b860e70d58bae31416691c0947095
sam <sam.charles.stanley@gmail.com>
2025-04-17 21:35:27 -0700

    restarting

--- src/alignment/alignment.cpp
+++ src/alignment/alignment.cpp
@@ -1,48 +1,18 @@
 #include "./alignment.hpp"
 
 namespace alignment {
-  int64_t execute_global_alignment(const std::string& string_one, const std::string& string_two) {
-    const size_t row_size = string_one.size() + 1;
-    const size_t col_size = string_two.size() + 1;
-    std::vector<dp_cell> cur_dp_row(col_size, dp_cell());
-    std::vector<dp_cell> prev_dp_row(col_size, dp_cell());
-
-    // temp
-    std::ofstream s("./output.txt");
-
-    // prev_dp_row[0].print_scores(s);
-    // s << " ";
-
-    for (size_t i = 1; i < col_size; ++i) {
-      prev_dp_row[i].set_top_row(i);
-      // prev_dp_row[i].print_scores(s);
-      // s << " ";
+  int64_t execute_global_alignment_bad(const std::string& string_one, const std::string& string_two) {
+    if (string_one.empty() || string_two.empty()) {
+      return 0;
     }
+    size_t max_row = string_one.size();
+    size_t max_col = string_one.size();
+    std::vector<std::vector<dp_cell>> dp_table(max_row, std::vector<dp_cell>(max_col, dp_cell()));
 
-    for (size_t row_idx = 1; row_idx < row_size; ++row_idx) {
-      cur_dp_row[0].set_left_col(row_idx);
-      // cur_dp_row[0].print_scores(s);
-      // s << " ";
-      for (size_t col_idx = 1; col_idx < col_size; ++col_idx) {
-        cur_dp_row[col_idx].score_cell(
-          prev_dp_row[col_idx-1],
-          prev_dp_row[col_idx],
-          cur_dp_row[col_idx-1],
-          string_one[row_idx-1],
-          string_two[col_idx-1]
-        );
-        // cur_dp_row[col_idx].print_scores(s);
-        // s << " ";
+    for (size_t i = 1; i < max_row; ++i) {
+      for (size_t j = 1; j < max_col; ++i) {
       }
-      // s << std::endl;
-      std::swap(prev_dp_row, cur_dp_row);
     }
-
-    s << prev_dp_row.back().to_string() << std::endl;
-
-    s.close();
-
-    return prev_dp_row.back().get_max_score();
   }
 
   // int32_t execute_modified_global_alignment(const std::string& string_one, const std::string& string_two) {
--- src/alignment/dp_cell.hpp
+++ src/alignment/dp_cell.hpp
@@ -18,58 +18,25 @@ class dp_cell {
 private:
   static constexpr int64_t min_score = static_cast<int64_t>(std::numeric_limits<int32_t>::min());
 
+  int64_t m_i_score;
   int64_t m_s_score;
   int64_t m_d_score;
-  int64_t m_i_score;
 
+  int32_t m_i_matches;
   int32_t m_s_matches;
   int32_t m_d_matches;
-  int32_t m_i_matches;
-
-  int32_t m_s_mismatches;
-  int32_t m_d_mismatches;
-  int32_t m_i_mismatches;
-
-  void score_s(const dp_cell&, const char, const char);
-  void score_d(const dp_cell&);
-  void score_i(const dp_cell&);
 public:
   dp_cell();
   ~dp_cell();
-
   int64_t get_max_score(void) const;
-  int32_t get_max_score_matches(void) const;
-  void score_cell(const dp_cell&, const dp_cell&, const dp_cell&, const char, const char);
-  void reset_cell(void);
 
-  void set_top_row(size_t col_idx);
-  void set_left_col(size_t row_idx);
-
+  int64_t get_i_score(void) const;
   int64_t get_s_score(void) const;
   int64_t get_d_score(void) const;
-  int64_t get_i_score(void) const;
 
-  int32_t get_s_matches(void) const;
-  int32_t get_d_matches(void) const;
-  int32_t get_i_matches(void) const;
-
-  int32_t get_s_mismatches(void) const;
-  int32_t get_d_mismatches(void) const;
-  int32_t get_i_mismatches(void) const;
-
-  void set_s_score(int64_t _s_score);
-  void set_d_score(int64_t _d_score);
-  void set_i_score(int64_t _i_score);
-
-  void set_s_matches(int32_t _s_matches);
-  void set_d_matches(int32_t _d_matches);
-  void set_i_matches(int32_t _i_matches);
-
-  void print_scores(std::ostream&) const;
-  void print_matches(std::ostream&) const;
-  void print_all(std::ostream&) const;
-
-  std::string to_string(void) const;
+  void set_i_score(const int64_t);
+  void set_i_score(const int64_t);
+  void set_i_score(const int64_t);
 };
 
 #endif
